11ad2b4b7bf8e03a37db5a3ec21bb86ce0074ecc
isaqkee <141652864+isaqkee@users.noreply.github.com>
2024-03-04 13:48:01 -0300

    Acompanhamento com o professor

---
+++ App.js
@@ -0,0 +1,24 @@
+import { View, Text, StyleSheet, Image } from "react-native";
+
+export default function App() {
+  return (
+    <View style={ESTILO.container}>
+      <Image source={require("./assets/logo.png")} style={ESTILO.logo}/>
+      <Text>
+        Meu app!
+      </Text>
+    </View>
+  )
+}
+
+const ESTILO = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#f3f3ff",
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  logo: {
+    marginBottom: 60
+  }
+})
