614111d18d8d8b821abb33a0bd4b1682e1e22eb1
isaqkee <141652864+isaqkee@users.noreply.github.com>
2024-03-25 15:03:00 -0300

    o básico ta feito, hora da morte

--- pages/index.js
+++ pages/index.js
@@ -3,13 +3,22 @@ import { ModalTokens } from '../components/modal';
 import Slider from "@react-native-community/slider";
 import { useState } from "react";
 
+let caracteres = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
 export function Home() {
     const [qtde, defineQtde] = useState(6);
     const [telaModal, configTelaModal]=useState(false);
+    const [tokenValue, configTokenValue] = useState("")
 
     function gerarToken() {
+        let token = ""
+        for (let i = 0, n = caracteres.length; i < qtde; i++) {
+            token += caracteres.charAt(Math.floor(Math.random() * n))
+        }
+        configTokenValue(token);
         configTelaModal(true);
         }
+        
     return (
         <View style={ESTILO.container}>
             <Image source={require("../assets/logo.png")} style={ESTILO.logo} />
@@ -34,7 +43,7 @@ export function Home() {
                 </Text>
             </TouchableOpacity>
             <Modal visible={telaModal} animationType="fade" transparent={true}>
-                <ModalTokens fechar={()=> configTelaModal(false)} />
+                <ModalTokens token={tokenValue} fechar={()=> configTelaModal(false)} />
             </Modal>
         </View>
 
--- pages/paginaSenhas.js
+++ pages/paginaSenhas.js
@@ -1,13 +1,45 @@
-import { View, StyleSheet, Text} from "react-native";
+import { View, StyleSheet, Text, FlatList } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import Armazenamento from '../hooks/bancoToken';
+import { useState, useEffect } from 'react';
+import { CaixaToken } from '../components/tokenView';
+import { useIsFocused } from '@react-navigation/native';
 
 export function PaginaSenhas() {
+    const [listaTokens, defListaTokens] = useState([]);
+    const { obterItem, removerItem } = Armazenamento();
+    const telaAtiva = useIsFocused();
+
+    useEffect(() => {
+        async function carregaTokens() {
+            const tokens = await obterItem("@token");
+            defListaTokens(tokens);
+        }
+        carregaTokens()
+    }, [telaAtiva]);
+
+    async function deletarToken(item) {
+        const tokens = await removerItem("@token", item)
+        defListaTokens(tokens)
+    };
+
     return (
         <SafeAreaView style={{ flex: 1 }}>
             <View style={ESTILO.header}>
                 <Text style={ESTILO.title}>
                     Minhas senhas
                 </Text>
+            </View>
+            <View style={ESTILO.content}>
+                <FlatList
+                    style={{ flex: 1, paddingTop: 14, }}
+                    data={listaTokens}
+                    keyExtractor={(item) => String(item)}
+                    renderItem={({ item }) => <CaixaToken
+                        token={item}
+                        removerToken={() => deletarToken(item)}
+                    />}
+                />
             </View>
         </SafeAreaView>
     )
@@ -23,6 +55,10 @@ const ESTILO = StyleSheet.create({
         fontSize: 18,
         fontWeight: "bold",
         color: "#FFF"
+    },
+    content: {
+        flex: 1,
+        paddingLeft: 14,
+        paddingRight: 14,
     }
-
 })
